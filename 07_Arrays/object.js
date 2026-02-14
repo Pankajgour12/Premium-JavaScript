0337a5586b1bfa15d0e63af6a8d47b493fc81520
pankajgour12 <pankajgour5000@gmail.com>
2026-02-14 13:23:14 +0530

    Objects

---
+++ 07_Arrays/object.js
@@ -0,0 +1,11 @@
+//! Object in js
+
+//? Object is a collection of key value pairs. It is a data structure that allows us to store and manipulate data in a structured way. Objects are used to represent real-world entities and their properties and behaviors.
+
+//? Creating an Object
+//? We can create an object using the Object constructor or using the object literal syntax.
+//? Using Object constructor
+let obj1 = new Object();
+
+//? Using object literal syntax
+let obj2 = {};  
