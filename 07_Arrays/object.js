df407ab8e756d60e9b8879880fc7db5ba6fca834
pankajgour12 <pankajgour5000@gmail.com>
2026-02-18 10:54:52 +0530

    Refactor object examples and enhance comments for clarity

--- 07_Arrays/object.js
+++ 07_Arrays/object.js
@@ -5,11 +5,36 @@
 //? Creating an Object
 //? We can create an object using the Object constructor or using the object literal syntax.
 //? Using Object constructor
-let obj1 = new Object();
+// let obj1 = new Object();
 
 //? Using object literal syntax
-let obj2 = {};  
+// let obj2 = {};  
 
 
-
+/* let obj ={
+    name: "alpha",
+    age: 24,
+    city: "bhagalpur",
+    isMarried: true,
+}
   
+let keys = Object.keys(obj);
+console.log(keys);
+
+let values = Object.values(obj);
+console.log(values);
+
+let entries = Object.entries(obj);
+console.log(entries); */
+
+
+let obj = {
+    name: "Pankaj",
+    age: 24,
+    city: "bhopal",
+    isMarried: false,
+}
+
+for(let key in obj){
+    console.log(key + ": " + obj[key]);
+}
