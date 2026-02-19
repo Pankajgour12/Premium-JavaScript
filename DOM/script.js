a6d2606837dc5e4852fe1bd2dfb89c6439e42d73
pankajgour12 <pankajgour5000@gmail.com>
2026-02-19 11:23:51 +0530

    DOM Started

---
+++ DOM/script.js
@@ -0,0 +1,15 @@
+
+let h1 = document.querySelector("h1")
+let a = document.querySelector("a")
+
+console.dir(h1)
+h1.innerHTML = `<i>I'm Pankaj Gour</i> `
+
+
+// create element 
+// append/ prepend karo jaha bhi element chahiye waha 
+let h2 = document.createElement("h2")
+h2.innerHTML = "Web Developer"
+document.body.append(h2)
+
+
