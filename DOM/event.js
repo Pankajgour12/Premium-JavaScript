2cdc42c72d1fba42b0536f0e3829f3a2f9ed3344
pankajgour12 <pankajgour5000@gmail.com>
2026-03-11 18:51:46 +0530

    file upload fn

--- DOM/event.js
+++ DOM/event.js
@@ -15,7 +15,7 @@ body.appendChild(div)
 
  
 // keydown 
-let h1 = document.querySelector("h1")
+/* let h1 = document.querySelector("h1")
 
 
 window.addEventListener("keydown",(data)=>{
@@ -25,6 +25,29 @@ window.addEventListener("keydown",(data)=>{
     else{
         h1.textContent = data.key
     }
+}) */
+
+
+    // file upload function 
+
+let inp = document.querySelector("#inp") 
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click",()=>{
+    inp.click();
+
+})
+
+inp.addEventListener("change",(data)=>{
+
+   const file = data.target.files[0]
+
+   if(file){
+    btn.textContent = file.name
+   }
+  
+   
+
 })
 
 
