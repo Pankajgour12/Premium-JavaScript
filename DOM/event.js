e2f3e7887ec6cc7a6fe067a5d9376a8e0ceb620f
pankajgour12 <pankajgour5000@gmail.com>
2026-03-11 17:46:46 +0530

    keydown event

--- DOM/event.js
+++ DOM/event.js
@@ -1,16 +1,36 @@
 
 // input events 
-let inp = document.querySelector("input");
+/* let inp = document.querySelector("input");
 let body = document.querySelector("body")
 
 const div = document.createElement('div');  
 
 inp.addEventListener("input",(evt)=>{
-//     if(evt.data !== null) console.log(evt.data);
+     if(evt.data !== null) console.log(evt.data);
 div.innerText = evt.data
 
 body.appendChild(div)
  
+}) */
+
+ 
+// keydown 
+let h1 = document.querySelector("h1")
+
+
+window.addEventListener("keydown",(data)=>{
+    if(data.key ===" "){
+        h1.textContent ="SPC"
+    }
+    else{
+        h1.textContent = data.key
+    }
 })
+
+
+
+
+
+
 
 
