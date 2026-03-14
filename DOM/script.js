1bbe2fbedc5f722cc1894c1578bab1c1ba782195
pankajgour12 <pankajgour5000@gmail.com>
2026-03-14 16:41:00 +0530

    setInterval , setTimeout

--- DOM/script.js
+++ DOM/script.js
@@ -1,29 +1,10 @@
-let form = document.querySelector("form")
-let email = document.querySelector("#email")
-let password = document.querySelector("#password")
+let count = 1
 
-let emailError = document.querySelector("#emailError")
-let passError = document.querySelector("#passwordError")
-
-form.addEventListener('submit',(e)=>{
-
-    e.preventDefault();
-
-    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
-    let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
-
-    let emailans = emailRegex.test(email.value);
-    let passans  = passwordRegex.test(password.value);
-
-    emailError.textContent = ""
-    passError.textContent = ""
-
-    if(!emailans){
-        emailError.textContent = "pls fill valid email"
+let interval = setInterval(()=>{
+    if(count <= 100){
+        console.log(count);
+        count++
     }
-
-    if(!passans){
-        passError.textContent = "pls fill valid password"
-    }
-
-})
+    else clearInterval(interval)
+  
+},100)
