42fb8572d09143bb3344410372d0f7b9680822e6
pankajgour12 <pankajgour5000@gmail.com>
2026-03-14 18:59:04 +0530

    toggle features by a button

--- DOM/localStorage/storage.js
+++ DOM/localStorage/storage.js
@@ -1,14 +1,26 @@
-// localStorage 
+/* // localStorage 
 //*  store kaise karna hai  --> setItem
 //* data fetch kaise karna hai --> getItem 
 //* remove kaise karna hai --> removeItem
 //* update kaise karna  hai --> 
 
 
-localStorage.setItem("name","Pankaj")
+/* localStorage.setItem("name","Pankaj")
 localStorage.setItem("name","Lotus")
 
- localStorage.removeItem("name")
+localStorage.removeItem("name")
+ */
+/* 
+document.cookie = "name = Pankaj"
+
+document.cookie = "age = 24 "
+
+ */
 
 
+/* 
+localStorage.setItem("friends",JSON.stringify(["pankaj","pnkj","lotus"]))
 
+
+let val = JSON.parse(localStorage.getItem("friends"))
+console.log(val); */ 
