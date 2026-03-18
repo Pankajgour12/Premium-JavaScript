b03e892e1b59327fa975732f164a7116bfa62ea7
pankajgour12 <pankajgour5000@gmail.com>
2026-03-18 19:32:12 +0530

    formValidation

--- DOM/call-app/script.js
+++ DOM/call-app/script.js
@@ -9,6 +9,18 @@ let formContainer = document.querySelector(".form-container");
 
 let closeForm = document.querySelectorAll(".closeForm")
 
+
+const form = document.querySelector("form");
+
+const imageInput = document.querySelector('input[placeholder="https://example.com/photo.jpg"]');
+const nameInput = document.querySelector('input[placeholder="Enter Full Name"]');
+const townInput = document.querySelector('input[placeholder="Enter Home Town"]');
+const purposeInput = document.querySelector('input[placeholder="e.g. Quick appointment"]');
+
+const categoryRadios = document.querySelectorAll('input[name="Category"]');
+
+const submitBtn = document.querySelector(".submit-btn");
+
  
 //form close of open 
 
@@ -18,3 +30,61 @@ addNote.addEventListener("click",()=>{
 addNote.addEventListener("click",()=>{
     formContainer.style.display = 'none'
 })
+
+
+
+
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const image = form.querySelectorAll("input")[0].value.trim();
+    const name = form.querySelectorAll("input")[1].value.trim();
+    const town = form.querySelectorAll("input")[2].value.trim();
+    const purpose = form.querySelectorAll("input")[3].value.trim();
+
+    const category = form.querySelector('input[name="Category"]:checked');
+
+    // validation start
+
+    if (!image || !name || !town || !purpose) {
+        alert("All fields are required");
+        return;
+    }
+
+    if (!image.startsWith("http")) {
+        alert("Invalid Image URL");
+        return;
+    }
+
+    if (name.length < 3) {
+        alert("Name must be at least 3 characters");
+        return;
+    }
+
+    if (purpose.length < 5) {
+        alert("Purpose too short");
+        return;
+    }
+
+    if (!category) {
+        alert("Select Category");
+        return;
+    }
+
+
+    
+    // success
+
+    console.log({
+        image,
+        name,
+        town,
+        purpose,
+        category: category.value
+    });
+
+    alert("Form Submitted Successfully");
+
+    form.reset();
+});
