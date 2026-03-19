e83b3b1c1924de1121d393881d1a2959177a53cc
pankajgour12 <pankajgour5000@gmail.com>
2026-03-19 18:39:22 +0530

    scope

---
+++ 10_Advance_JS/script.js
@@ -0,0 +1,9 @@
+ //* scope - function scope, global scope and block scope 
+
+
+ //! function scope - function ke ander hi use ho sakti hai 
+
+
+ //!  global scope - poore code mein kahi bhi use ho sakti he 
+
+ //! block scope - { } - curly braces main hi use ho sakti he
