66425d24807c3856eb382c4c529dfc206d90ee6d
pankajgour12 <pankajgour5000@gmail.com>
2026-03-21 18:45:51 +0530

    loops

--- 09_DOM Manupulation/AdvanceDOM/array.js
+++ 09_DOM Manupulation/AdvanceDOM/array.js
@@ -1,6 +1,30 @@
 
 
-const languages =['javascript', 'python', 'c', "java", "c++"]
-
+/* const languages =['javascript', 'python', 'c', "java", "c++"]
+ */
 //? .length 
-console.log(languages.length) // output : 5
+// console.log(languages.length) // output : 5
+
+const actor = [
+    {
+        name: 'actor 1',
+        payment: 200
+    },
+    {
+        name: 'actor 2',
+        payment: 100
+    },
+    {
+        name: 'actor 3',
+        payment: 300
+    },
+
+]
+
+
+for( let i= 0 ; i< actor.length ; i++){
+    console.log(actor[i].name);
+    console.log(actor[i].payment);
+
+}
+
