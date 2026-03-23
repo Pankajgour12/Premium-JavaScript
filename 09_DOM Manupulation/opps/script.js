8a4126da52223a51f28a0967c9f67a58d2e854a3
pankajgour12 <pankajgour5000@gmail.com>
2026-03-23 16:53:37 +0530

    class prototype

--- 09_DOM Manupulation/opps/script.js
+++ 09_DOM Manupulation/opps/script.js
@@ -1,4 +1,4 @@
-function CreateUser(name, age, color) {
+/* function CreateUser(name, age, color) {
   this.name = name;
   this.age = age;
   this.color = color;
@@ -29,4 +29,34 @@ function CreateUser(name, age, color) {
 let user1 = new CreateUser("Pankaj", 24, "#de87ed" );
 let user2 = new CreateUser("Subham", 16, "yellow");
 
-;
+; */
+
+
+
+
+class CreateUser {
+      constructor(name, age, color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+      }
+      write(text) {
+        this.company = "Amazon";
+        let div = document.createElement("div");
+        let h1 = document.createElement("h1");
+        let p = document.createElement("p");
+        h1.textContent = `${text}  ${this.name} `;
+        h1.style.color = this.color;
+        h1.style.textAlign = "center";
+        p.textContent = `Age: ${this.age} || Company: ${this.company}`;        p.style.color = "pink";
+        p.style.textAlign = "center";
+        
+        div.appendChild(h1);
+        div.appendChild(p);
+        document.body.appendChild(div);
+        }
+    }
+    let user1 = new CreateUser("Pankaj", 24, "#de87ed" );
+    let user2 = new CreateUser("Lotus", 22, "#e39b53" );
+
+
