8a6d52da595abafca522d4ec09b9142d2457c6bf
pankajgour12 <pankajgour5000@gmail.com>
2026-03-23 10:22:12 +0530

    this

--- 09_DOM Manupulation/opps/script.js
+++ 09_DOM Manupulation/opps/script.js
@@ -1,20 +1,22 @@
-function CreateUser(name, age, color, company) {
+function CreateUser(name, age, color) {
   this.name = name;
   this.age = age;
   this.color = color;
-  this.company = company;
+  this.company ="Google";
 
-  this.write = function () {
+  this.write = function (text) {
     let div = document.createElement("div");
 
     let h1 = document.createElement("h1");
     let p = document.createElement("p");
 
-    h1.textContent = `heyyy ${this.name} kaise ho bhai`;
+    h1.textContent = `${text}  ${name} `;
     h1.style.color = this.color;
     h1.style.textAlign = "center";
 
-    p.textContent = `Age: ${this.age} | Company: ${this.company}`;
+    p.textContent = `Age: ${age} | Company: ${this.company}`;
+    p.style.color = "pink";
+    p.style.textAlign = "center";
 
     div.appendChild(h1);
     div.appendChild(p);
