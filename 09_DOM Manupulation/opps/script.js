611d3bae92ef0bbf07a5726ef8c37ae4c266c16a
pankajgour12 <pankajgour5000@gmail.com>
2026-03-24 20:02:54 +0530

    async function

--- 09_DOM Manupulation/opps/script.js
+++ 09_DOM Manupulation/opps/script.js
@@ -32,25 +32,31 @@ let admin = new Admin("admin","pnlka@gmail.com",24)
  */
 
 
-
-let pr = new Promise (function(res,rej){
+/* 
+ let pr = new Promise (function(res,rej){
     setTimeout(()=>{
         let rn = Math.floor(Math.random()*10)
 
         if(rn>5) res("resolved with "+ rn)
         else rej("rejected with "+ rn)
     
-    },3000)
-}).then(function(val){
-    console.log(val)
-}).catch(function(val){
-    console.log(val)
+    },1000)
 })
 
+async function abcd() {
+    try {
+        let val = await pr
+        console.log(val)    
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+abcd();
 
 
-
-
+ */
 
 
 
