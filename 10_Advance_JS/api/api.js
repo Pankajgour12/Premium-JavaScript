4dae18d46e651b8cabfc40fc117df1972f32a098
pankajgour12 <pankajgour5000@gmail.com>
2026-03-31 22:25:12 +0530

    dummy api call

--- 10_Advance_JS/api/api.js
+++ 10_Advance_JS/api/api.js
@@ -1,16 +1,13 @@
 const container = document.getElementById("container");
 
-// async function
 async function fetchProducts() {
   try {
-    // API call
-    const res = await fetch("https://fakestoreapi.com/products");
- console.log(res.data);
-    // JSON convert
+    const res = await fetch("https://dummyjson.com/products");
+
     const data = await res.json();
 
-    // loop
-    data.forEach((item) => {
+    // IMPORTANT: yaha data.products use karna padega
+    data.products.forEach((item) => {
 
       const card = document.createElement("div");
 
@@ -18,23 +15,21 @@ async function fetchProducts() {
         "w-80 bg-white rounded-xl shadow-lg overflow-hidden hover:scale-105 transition duration-300";
 
       card.innerHTML = `
-        <img src="${item.image}" class="w-full h-48 object-contain p-4 bg-white"/>
+        <img src="${item.thumbnail}" class="w-full h-48 object-cover"/>
 
         <div class="p-4">
-          <h1 class="text-lg font-bold text-gray-800 line-clamp-1">
+          <h1 class="text-lg font-bold text-gray-800">
             ${item.title}
           </h1>
 
-          <p class="text-sm text-gray-600 mt-2 line-clamp-2">
-            ${item.description}
+          <p class="text-sm text-gray-600 mt-2">
+            ${item.description.substring(0, 80)}...
           </p>
 
           <div class="flex justify-between items-center mt-4">
             <span class="text-xl font-bold text-green-600">$${item.price}</span>
 
-            <button class="px-3 py-1 bg-black text-white rounded hover:bg-gray-800">
-              Buy
-            </button>
+            <span class="text-yellow-500">⭐ ${item.rating}</span>
           </div>
         </div>
       `;
@@ -47,5 +42,4 @@ async function fetchProducts() {
   }
 }
 
-// function call
 fetchProducts();
